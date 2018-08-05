84ec1d4f0a184a66b90082b39029f9af4f7ee9dd
nanto88 <leksono.nanto@gmail.com>
2018-08-05 13:32:45 +0700

    pickle to joblib, nort to rt

--- myproject.py
+++ myproject.py
@@ -4,18 +4,13 @@ from mining.stopwords import preprocess_tweet
 from mining.dateformat import sinceDate, untilDate
 from sklearn.externals import joblib
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-import pickle
 import tweepy
 import time
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(__name__)
 
-
 api = twitterApi()
-
-
-
 
 def t():
     return round(time.clock(), 2)
@@ -39,44 +34,47 @@ def sentiment():
     
     
     if request.method=='POST':
-        query = request.form['query']
-        daterange = request.form['daterange']
-        nort = request.form.get('nort')
-        stem = request.form.get('stem')
         
         t0 = time.clock()
         
+        query = request.form['query']
+        daterange = request.form['daterange']
+        rt = request.form.get('rt')
+        stem = request.form.get('stem')
+        
+        query = query+"-filter:retweets"
+                       
         if stem:
             with open('static/pilgubstem2.pkl', 'rb') as f:
-                model = pickle.load(f)
+                model = joblib.load(f)
             def clean(tweet):
                 factory = StemmerFactory()
                 stemmer = factory.create_stemmer()
                 clean = stemmer.stem(preprocess_tweet(tweet))
                 return clean
             app.jinja_env.globals.update(clean=clean)
-            t1 = time.clock()
         else:
             with open('static/pilgub.pkl', 'rb') as f:
-                model = pickle.load(f)
+                model = joblib.load(f)
             def clean(tweet):
                 return preprocess_tweet(tweet)
             app.jinja_env.globals.update(clean=clean)
-            t1 = time.clock()
         
-        est = round(t1, 2)-round(t0, 2)
-        
-        if nort:
-            query=query+"-filter:retweets"
+        if rt:
+            query=query[:-16]
         
         if daterange=="":
             tweets = tweepy.Cursor(api.search, q=query, lang="id", show_user="true").items(100)
         else:
-            tweets = tweepy.Cursor(api.search, q=query, lang="id", show_user="true", since=sinceDate(daterange), until=untilDate(daterange)).items(100)
+            tweets = tweepy.Cursor(api.search, q=query, lang="id", show_user="true", 
+                                   since=sinceDate(daterange), until=untilDate(daterange)).items(100)
+            
+        t1 = time.clock()
+        
+        est = round(t1, 2)-round(t0, 2)
+
       
     return render_template('sentiment.html', tweets=tweets, model=model, est=est, query=query)
-
-
 
 
 if __name__ == '__main__':
